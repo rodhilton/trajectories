98ac3b33806874129fb0696c66a189d73509e8ec
Rod Hilton <rod.hilton@gmail.com>
2012-12-13 14:03:02 -0700

    added filtering

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -13,8 +13,13 @@ class BoardTest {
     def shouldToString() {
         val b = Board(5)
         val b2 = b.set(('a', 3), 0)
-        //Manual Inspection required, this is for display purposes
-        println(b2.toString)
+
+        val s = b2.toString
+        assertTrue(s contains "[0][ ][ ][ ][ ]  3")
+        assertTrue(s contains "[ ][ ][ ][ ][ ]  5")
+        assertTrue(s contains " a  b  c  d  e ")
+
+//        println(b2.toString)
     }
 
     @Test
@@ -47,8 +52,45 @@ class BoardTest {
         assertEquals(b('b', 2), 2)
     }
 
+    @Test(expected = classOf[AssertionError])
+    def shouldDisallowAddingDifferentSizes() {
+        val a = Board(5)
+        val b = Board(6)
+
+        a.plus(b)
+    }
+
     @Test
     def shouldAdd() {
-//        val a = Board(5).set()
+        val a = Board(5).set(('c', 2) -> 5, ('b', 1) -> 2)
+        val d = Board(5).set(('c', 2) -> 2, ('d', 5) -> 3)
+
+        val c = a.plus(d)
+
+        assertEquals(c('b', 1), 2)
+        assertEquals(c('c', 2), 7) // These two cells should be added together
+        assertEquals(c('d', 5), 3)
+
+        assertEquals(a+d, c)
+    }
+
+//    @Test(expected = classOf[AssertionError])
+//    def shouldDisallowIntersectingDifferentSizes() {
+//        val a = Board(5)
+//        val b = Board(6)
+//
+//        a.intersect(b)
+//    }
+
+    @Test
+    def shouldFilter() {
+        val a = Board(5).set(('c', 2) -> 5, ('b', 1) -> 1)
+
+        val c = a.filter{ case ((col, row), value) =>
+            value > 1
+        }
+
+        assertEquals(c('c', 2), 5)
+        assertTrue(c.get('b',1).isEmpty)
     }
 }
