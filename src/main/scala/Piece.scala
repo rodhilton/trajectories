a21e84bd97e8bf0459ba22662d50192826fe8281
Rod Hilton <rod.hilton@gmail.com>
2012-12-13 22:57:00 -0700

    added cropping, created packaging

--- src/main/scala/Piece.scala
+++ src/main/scala/Piece.scala
@@ -1,3 +1,5 @@
+import com.nomachetejuggling.trajectories.board.Board
+
 trait Piece {
 
     def bigTable: Board = {
--- src/main/scala/com/nomachetejuggling/trajectories/board/Board.scala
+++ src/main/scala/com/nomachetejuggling/trajectories/board/Board.scala
@@ -1,3 +1,5 @@
+package com.nomachetejuggling.trajectories.board
+
 import collection.immutable.IndexedSeq
 import java.util.NoSuchElementException
 
@@ -10,9 +12,12 @@ trait Board {
     def intersect(otherBoard: Board): Board
     def filter(f: Tuple2[Tuple2[Char, Int], Int] => Boolean): Board
     def activeSpaces: Set[Tuple2[Char,Int]]
+    def allSpaces: Set[Tuple2[Char,Int]]
+    def crop(topLeft: Tuple2[Char, Int], bottomRight: Tuple2[Char, Int]): Board
     def overlay(otherBoard: Board, mySpot: Tuple2[Char, Int], otherSpot: Tuple2[Char, Int], joiner: ((Int, Int)=>Int) = {(i,j)=>i+j}): Board
     def contains(coords: Tuple2[Char, Int]): Boolean
     def isDefinedAt(coords: Tuple2[Char, Int]): Boolean
+    def toMap: Map[Tuple2[Char, Int], Int]
 
     //Convenience Interface:
     def +(otherBoard: Board): Board = this.plus(otherBoard)
@@ -38,13 +43,26 @@ trait Board {
 object Board {
     def apply(size: Int): Board = new BoardImpl(size, Map.empty)
 
+    def insideBounds(topLeft: Tuple2[Char, Int], bottomRight: Tuple2[Char, Int], coordsToCheck: Tuple2[Char, Int]): Boolean = {
+        assertValidZone(topLeft, bottomRight)
+        (topLeft._1 to bottomRight._1).contains(coordsToCheck._1) && (bottomRight._2 to topLeft._2).contains(coordsToCheck._2)
+    }
+
+    protected def assertValidZone(topLeft: Tuple2[Char, Int], bottomRight: Tuple2[Char, Int]) {
+        //This looks reversed because the numbers start at the bottom but the letters start on the right
+        assert(bottomRight._1 >= topLeft._1 && topLeft._2 >= bottomRight._2, "Crop coordinates not valid: "+topLeft+" to "+bottomRight)
+        assert(bottomRight._1 - topLeft._1 == topLeft._2 - bottomRight._2, "Asked to crop to a nonsquare: "+(bottomRight._1 - topLeft._1)+"x"+(topLeft._2 - bottomRight._2))
+    }
+
     //TODO: figure out why I can't do this
     //def apply(size: Int, pairs: Tuple2[Tuple2[Char, Int], Int]*): Board = Board(size).set(pairs)
 
     case class BoardImpl(override val size: Int, boardSpace: Map[Tuple2[Char, Int], Int]) extends Board {
         val columns = ('a' to ('a' to 'z')(size - 1))
         val rows = (1 to size).reverse
-        val allPossibleSpaces = columns.flatMap(c => rows.map(r => (c, r)))
+        override lazy val allSpaces = columns.flatMap(c => rows.map(r => (c, r))).toSet
+
+        override lazy val toMap: Map[Tuple2[Char, Int], Int] = boardSpace
 
         override def set(coordinates: Tuple2[Char, Int], value: Int): Board = new BoardImpl(size, boardSpace.updated(coordinates, value))
 
@@ -84,11 +102,36 @@ object Board {
 
         override def filter(f: Tuple2[Tuple2[Char, Int], Int] => Boolean): Board = new BoardImpl(size, boardSpace.filter(f))
 
-        override def overlay(otherBoard: Board, mySpot: Tuple2[Char, Int], otherSpot: Tuple2[Char, Int], joiner: ((Int, Int)=>Int)): Board = {
+        override def overlay(smallBoard: Board, mySpot: Tuple2[Char, Int], otherSpot: Tuple2[Char, Int], joiner: ((Int, Int)=>Int)): Board = {
             assert(this.contains(mySpot), "Local board did not contain spot "+mySpot+", size is "+size)
-            assert(otherBoard.contains(otherSpot), "Other board did not contain spot "+otherSpot+", size is "+otherBoard.size)
+            assert(smallBoard.contains(otherSpot), "Other board did not contain spot "+otherSpot+", size is "+smallBoard.size)
+
+            val colOffset = otherSpot._1 - mySpot._1
+            val rowOffset = otherSpot._2 - mySpot._2
+
+
 
             this
+
+        }
+
+        override def crop(topLeft: Tuple2[Char, Int], bottomRight: Tuple2[Char, Int]): Board = {
+            assert(this.contains(topLeft) && this.contains(bottomRight), "Asked to crop from "+topLeft+" to "+bottomRight+", but my size is "+size)
+            assertValidZone(topLeft, bottomRight)
+
+
+            //Remove everything outside of our bounds, leaving only the correct elements (with the wrong indexes)
+            val inside = insideBounds(topLeft, bottomRight, _: (Char, Int))
+
+            val filteredBoard = this.filter{ case (coords, value) =>
+                inside(coords)
+            }
+
+            val offsetMap = filteredBoard.toMap.collect{ case ((col, row), value) =>
+                (((col-topLeft._1)+'a'.toInt).toChar, row-bottomRight._2+1) -> value
+            }
+
+            new BoardImpl(topLeft._2 - bottomRight._2 + 1, offsetMap)
         }
 
         override def toString() = {
