1869ed9bbce15658dbd5ef12862518cbb22d8d48
Rod Hilton <rod.hilton@gmail.com>
2012-12-13 18:18:20 -0700

    more progress, need to write overlay func (hard one)

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -7,11 +7,18 @@ trait Board {
     def set(coordinates: Tuple2[Char, Int], value: Int): Board
     def get(coordinates: Tuple2[Char, Int]): Option[Int]
     def plus(otherBoard: Board): Board
+    def intersect(otherBoard: Board): Board
     def filter(f: Tuple2[Tuple2[Char, Int], Int] => Boolean): Board
+    def activeSpaces: Set[Tuple2[Char,Int]]
+    def overlay(otherBoard: Board, mySpot: Tuple2[Char, Int], otherSpot: Tuple2[Char, Int], joiner: ((Int, Int)=>Int) = {(i,j)=>i+j}): Board
+    def contains(coords: Tuple2[Char, Int]): Boolean
+    def isDefinedAt(coords: Tuple2[Char, Int]): Boolean
 
     //Convenience Interface:
     def +(otherBoard: Board): Board = this.plus(otherBoard)
-
+    def &(otherBoard: Board): Board = this.intersect(otherBoard)
+    def contains(c:Char, i:Int): Boolean = this.contains((c,i))
+    def isDefinedAt(c:Char, i:Int): Boolean = this.isDefinedAt((c,i))
     def set(pairs: Tuple2[Tuple2[Char, Int], Int]*): Board = {
         pairs.foldLeft(this) {
             case (b: Board, pair: Tuple2[Tuple2[Char, Int], Int]) =>
@@ -44,22 +51,45 @@ object Board {
         override def get(coordinates: Tuple2[Char, Int]): Option[Int] = boardSpace.get(coordinates)
 
         override def plus(otherBoard: Board): Board = {
-            assert(size == otherBoard.size)
+            assert(size == otherBoard.size, "Tried to add two boards of unequal size. Me: "+size+", other: "+otherBoard.size)
 
-            val allSpacesAsOptionalTuples = allPossibleSpaces.map {
+            val spacesAsTuples = ( activeSpaces ++ otherBoard.activeSpaces).map {
                 coords =>
                     (this.get(coords), otherBoard.get(coords)) match {
-                        case (Some(left), Some(right)) => Some((coords, left + right))
-                        case (Some(left), None) => Some((coords, left))
-                        case (None, Some(right)) => Some((coords, right))
-                        case (None, None) => None
+                        case (Some(left), Some(right)) => coords->(left+right)
+                        case (Some(left), None) => coords->left
+                        case (None, Some(right)) => coords->right
+                        case (None, None) => throw new Exception("This can't happen, each space has to be in one of the two maps")
                     }
             }
 
-            new BoardImpl(size, allSpacesAsOptionalTuples.flatten.toMap)
+            new BoardImpl(size, spacesAsTuples.toMap)
         }
 
+        override def intersect(otherBoard: Board): Board = {
+            assert(size == otherBoard.size)
+
+            //TODO: get rid of this ugly duplication from plus()
+            val spacesAsTuples = ( activeSpaces & otherBoard.activeSpaces).map {
+                coords => coords -> scala.math.max(this(coords), otherBoard(coords))
+            }
+
+            new BoardImpl(size, spacesAsTuples.toMap)
+        }
+
+        def contains(coords: Tuple2[Char, Int]): Boolean = columns.contains(coords._1) && rows.contains(coords._2)
+        def isDefinedAt(coords: Tuple2[Char, Int]): Boolean = this.boardSpace.isDefinedAt(coords)
+
+        override def activeSpaces: Set[Tuple2[Char,Int]] = boardSpace.keySet
+
         override def filter(f: Tuple2[Tuple2[Char, Int], Int] => Boolean): Board = new BoardImpl(size, boardSpace.filter(f))
+
+        override def overlay(otherBoard: Board, mySpot: Tuple2[Char, Int], otherSpot: Tuple2[Char, Int], joiner: ((Int, Int)=>Int)): Board = {
+            assert(this.contains(mySpot), "Local board did not contain spot "+mySpot+", size is "+size)
+            assert(otherBoard.contains(otherSpot), "Other board did not contain spot "+otherSpot+", size is "+otherBoard.size)
+
+            this
+        }
 
         override def toString() = {
             val rowStrings = for {
--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -1,5 +1,4 @@
 import org.junit.Test
-import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Assert._
 
 class BoardTest {
@@ -52,6 +51,13 @@ class BoardTest {
         assertEquals(b('b', 2), 2)
     }
 
+    @Test
+    def shouldSupplySpaces() {
+        val b = Board(5).set(('a', 3) -> 1, ('b', 2) -> 2)
+
+        assertEquals(b.activeSpaces, Set(('a',3), ('b',2)))
+    }
+
     @Test(expected = classOf[AssertionError])
     def shouldDisallowAddingDifferentSizes() {
         val a = Board(5)
@@ -74,13 +80,27 @@ class BoardTest {
         assertEquals(a+d, c)
     }
 
-//    @Test(expected = classOf[AssertionError])
-//    def shouldDisallowIntersectingDifferentSizes() {
-//        val a = Board(5)
-//        val b = Board(6)
-//
-//        a.intersect(b)
-//    }
+    @Test(expected = classOf[AssertionError])
+    def shouldDisallowIntersectingDifferentSizes() {
+        val a = Board(5)
+        val b = Board(6)
+
+        a.intersect(b)
+    }
+
+    @Test
+    def shouldIntersect() {
+        val a = Board(5).set(('c', 2) -> 5, ('b', 1) -> 2)
+        val d = Board(5).set(('c', 2) -> 2, ('d', 5) -> 3)
+
+        val c = a.intersect(d)
+
+        assertTrue(c.get('b', 1).isEmpty)
+        assertEquals(c('c', 2), 5) //The maximum
+        assertTrue(c.get('d', 5).isEmpty)
+
+        assertEquals(a & d, c)
+    }
 
     @Test
     def shouldFilter() {
@@ -92,5 +112,93 @@ class BoardTest {
 
         assertEquals(c('c', 2), 5)
         assertTrue(c.get('b',1).isEmpty)
+    }
+
+    @Test
+    def considersSpacesWithNoValues() {
+        val a = Board(3).set(('b',2)->0)
+
+        assertTrue(a.contains(('a',1)))
+        assertTrue(a.contains('b',2))
+        assertFalse(a.isDefinedAt(('a',1)))
+        assertTrue(a.isDefinedAt('b',2))
+    }
+
+
+
+    /**
+     * This one is complicated.  Lets say I have a large board:
+     *
+     * [ ][2][ ] 3
+     * [ ][0][1] 2  3x3
+     * [6][2][3] 1
+     *  a  b  c
+     *
+     * And I want to overlay on top of it a smaller board:
+     *
+     * [0][1] 2  2x2
+     * [2][4] 1
+     *  a  b
+     *
+     * I have to declare what point is mapping to what point.
+     *
+     * So the big board b2 will map to the small board a2.  In the process of overlaying, the function will also take a joining function, like +, which will add points together
+     *
+     * [0][2] 2
+     * [4][7] 1
+     *  a  b
+     *
+     */
+    @Test
+    def shouldOverlay() {
+
+        val a = Board(3).set(
+            ('b',3)->2,
+            ('b',2)->0,
+            ('c',2)->1,
+            ('a',1)->6,
+            ('b',1)->2,
+            ('c',1)->3
+        )
+
+        val b = Board(3).set(
+            ('b',3)->2,
+            ('b',2)->0,
+            ('c',2)->1,
+            ('a',1)->6,
+            ('b',1)->2,
+            ('c',1)->3
+        )
+
+        //TODO: make sure you dont overlap a larger board
+        //TODO: make sure you can't overlap a board such that it falls off (is that worth doing?  seems like that's alright, it should intersect)
+        //TODO: make sure you don't specify overlap spots not on the boards
+
+        val c = a.overlay(b, ('b',2), ('a',2))
+
+        assertEquals(c.size, 2)
+        assertEquals(c('a', 1), 4)
+        assertEquals(c('a', 2), 0)
+        assertEquals(c('b', 1), 7)
+        assertEquals(c('b', 2), 2)
+
+    }
+
+    @Test(expected=classOf[AssertionError])
+    def cannotOverlaySpotNotOnBigBoard() {
+
+        val a = Board(3)
+        val b = Board(2)
+
+        a.overlay(b, ('d',1), ('a',1))
+    }
+
+    @Test(expected=classOf[AssertionError])
+    def cannotOverlaySpotNotOnSmallBoard() {
+
+        val a = Board(3)
+        val b = Board(2)
+
+        a.overlay(b, ('a',1), ('c',1))
     }
 }
