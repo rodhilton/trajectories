04113a15867cdb6dc7e8b6cc4887c50a1ae53484
Rod Hilton <rod.hilton@gmail.com>
2012-12-15 01:06:08 -0700

    Added a ton of junk to the parsing and main stuff

---
+++ src/main/scala/com/nomachetejuggling/trajectories/Main.scala
@@ -0,0 +1,94 @@
+package com.nomachetejuggling.trajectories
+
+import org.apache.commons.cli.{Option => Opt, _}
+
+object Main {
+    def main(args: Array[String]) {
+        new Main().execute(args)
+    }
+}
+
+class Main() {
+
+    def execute(args: Array[String]) {
+        val pieceOpt = new Opt("p", "piece", true, "The piece to use (pawn, king, queen, knight, bishop, rook")
+        val startOpt = new Opt("s", "start", true, "The space to start (a1, b6, ...)")
+        val destOpt = new Opt("d", "dest", true, "The space to end (a2, c8, ...)")
+        val sizeOpt = new Opt("z", "size", true, "Size of the board (must be <= 26")
+
+        val options = new Options()
+
+        options.addOption(pieceOpt)
+        options.addOption(startOpt)
+        options.addOption(destOpt)
+        options.addOption(sizeOpt)
+
+        // create the parser
+        val parser = new GnuParser()
+        try {
+            // parse the command line arguments
+            val line = parser.parse(options, args)
+
+            if (line.hasOption("piece") && line.hasOption("start") && line.hasOption("dest")) {
+                val startInput: String = line.getOptionValue("start")
+                val destInput: String = line.getOptionValue("dest")
+                val pieceInput: String = line.getOptionValue("piece")
+                val sizeInput: String = line.getOptionValue("size")
+
+                val startCoord = parseShortCode(startInput)
+                val destCoord = parseShortCode(destInput)
+                val piece = parsePiece(pieceInput)
+                val size = parseSize(sizeInput)
+
+                if (startCoord.isEmpty || destCoord.isEmpty) throw new ParseException("Coordinates are in invalid format")
+                if (piece.isEmpty) throw new ParseException("No piece known by name " + pieceInput)
+
+                val pathFinder = new PathFinder(piece.get)
+
+                val paths = pathFinder.getPaths(Board(size), startCoord.get, destCoord.get)
+
+                println("There are "+paths.size+" shortest trajectories from "+startInput+" to "+destInput+".")
+                println("Here's one of them: \n")
+
+                println(util.Random.shuffle(paths).head.toFullBoardString)
+
+            } else {
+                throw new ParseException("Incomplete arguments")
+            }
+        }
+        catch {
+            case exp: ParseException => {
+                System.err.println("Failure: " + exp.getMessage)
+                val formatter = new HelpFormatter()
+                formatter.printHelp("trajectories", options)
+            }
+        }
+    }
+
+    private def parseSize(s: String): Int = {
+        s match {
+            case s: String if (1 to 26).contains(s.toInt) => s.toInt
+            case _ => 8
+        }
+    }
+
+    private def parsePiece(s: String): Option[Piece] = {
+        s.toLowerCase match {
+            case "pawn" => Some(Pieces.pawn)
+            case "king" => Some(Pieces.king)
+            case "queen" => Some(Pieces.queen)
+            case "rook" => Some(Pieces.rook)
+            case "bishop" => Some(Pieces.bishop)
+            case "knight" => Some(Pieces.knight)
+            case _ => None
+        }
+    }
+
+    private def parseShortCode(s: String): Option[Coordinates] = {
+        val ShortCodePattern = """([a-z])(\d)""".r
+        s.toLowerCase match {
+            case ShortCodePattern(col, row) => Some((col.head, row.toInt))
+            case _ => None
+        }
+    }
+}
