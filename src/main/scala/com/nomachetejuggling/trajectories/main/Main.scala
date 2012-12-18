646ed21606f2f61fb53949e5390f31b33e26393e
Rod Hilton <rod.hilton@gmail.com>
2012-12-17 22:54:09 -0700

    slight cleanup in Main

--- src/main/scala/com/nomachetejuggling/trajectories/main/Main.scala
+++ src/main/scala/com/nomachetejuggling/trajectories/main/Main.scala
@@ -10,50 +10,47 @@ object Main {
 }
 
 class Main() {
+    val PIECE = "piece"
+    val START = "start"
+    val DEST= "dest"
+    val SIZE = "size"
+    val ALL = "all"
+    val HELP = "help"
+    val ILLEGAL = "illegal"
 
     def execute(args: Array[String]) {
-        val pieceOpt = new Opt("p", "piece", true, "The piece to use (pawn, king, queen, knight, bishop, rook, weird)")
-        val startOpt = new Opt("s", "start", true, "The space to start (a1, b6, ...)")
-        val destOpt = new Opt("d", "dest", true, "The space to end (a2, c8, ...)")
-        val sizeOpt = new Opt("z", "size", true, "Size of the board (must be <= 26)")
-        val allOpt = new Opt("a", "all", false, "Output information about all paths (can be slow)")
-        val helpOpt = new Opt("h", "help", false, "Print this help")
-        val illegalOpt = new Opt("i", "illegal", true, "Input spaces that are illegal separated by commas (a2,b3,e6)")
 
         val options = new Options()
 
-        options.addOption(pieceOpt)
-        options.addOption(startOpt)
-        options.addOption(destOpt)
-        options.addOption(sizeOpt)
-        options.addOption(allOpt)
-        options.addOption(helpOpt)
-        options.addOption(illegalOpt)
+        options.addOption(new Opt("p", PIECE, true, "The piece to use (pawn, king, queen, knight, bishop, rook, weird)"))
+        options.addOption(new Opt("s", START, true, "The space to start (a1, b6, ...)"))
+        options.addOption(new Opt("d", DEST, true, "The space to end (a2, c8, ...)"))
+        options.addOption(new Opt("z", SIZE, true, "Size of the board (must be <= 26)"))
+        options.addOption(new Opt("a", ALL, false, "Output information about all paths (can be slow)"))
+        options.addOption(new Opt("h", HELP, false, "Print this help"))
+        options.addOption(new Opt("i", ILLEGAL, true, "Input spaces that are illegal separated by commas (a2,b3,e6)"))
 
-        // create the parser
         val parser = new GnuParser()
         try {
-            // parse the command line arguments
             val line = parser.parse(options, args)
-            if (line.hasOption("help")) throw new ParseException("Help Requested")
+            if (line.hasOption(HELP)) throw new ParseException("Help Requested")
 
-            if (line.hasOption("piece") && line.hasOption("start") && line.hasOption("dest")) {
-                val startInput: String = line.getOptionValue("start")
-                val destInput: String = line.getOptionValue("dest")
-                val pieceInput: String = line.getOptionValue("piece")
-                val sizeInput: String = line.getOptionValue("size")
-                val illegalInput: String = line.getOptionValue("illegal")
+            if (line.hasOption(PIECE) && line.hasOption(START) && line.hasOption(DEST)) {
+                val startInput: String = line.getOptionValue(START)
+                val destInput: String = line.getOptionValue(DEST)
+                val pieceInput: String = line.getOptionValue(PIECE)
+                val sizeInput: String = line.getOptionValue(SIZE)
+                val illegalInput: String = line.getOptionValue(ILLEGAL)
 
                 val startCoord = parseShortCode(startInput)
                 val destCoord = parseShortCode(destInput)
                 val piece = parsePiece(pieceInput)
                 val size = parseSize(sizeInput)
                 val illegal = parseIllegal(illegalInput)
-                val all = line.hasOption("all")
+                val all = line.hasOption(ALL)
 
                 if (startCoord.isEmpty || destCoord.isEmpty) throw new ParseException("Coordinates are in invalid format")
                 if (piece.isEmpty) throw new ParseException("No piece known by name " + pieceInput)
-                //if (all && size > 10) throw new ParseException("Cannot request all paths on extremely large boards")
 
                 val pathFinder = new PathFinder(piece.get)
 
@@ -103,10 +100,7 @@ class Main() {
 
     private def parseIllegal(s: String): Set[Coordinates] = {
         try {
-            val illegalList = s.split(",")
-            illegalList.collect {
-                case shortCode: String => parseShortCode(shortCode)
-            }.flatten.toSet
+            s.split(",").flatMap (code => parseShortCode(code)).toSet
         }
         catch {
             case _: Exception => Set.empty
